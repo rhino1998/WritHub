7265d42804a2313c086d544cbf4682c56b88fae0
Riley Wilburn <rhino1998@gmail.com>
2018-02-20 03:03:39 -0500

    Added cmd args instead of hardcoding

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
 	"log"
+	"os"
 	"time"
 
+	"github.com/rhino1998/writhub/tiny"
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
 func main() {
 
-	repo, err := git.PlainInit("tmp", false)
+	repo, err := git.PlainInit(os.Args[1], false)
 	if err != nil {
-		repo, err = git.PlainOpen("tmp")
+		repo, err = git.PlainOpen(os.Args[1])
 		if err != nil {
 			log.Fatalf("%v\n", err)
 		}
@@ -21,22 +25,37 @@ func main() {
 
 	wt, err := repo.Worktree()
 	if err != nil {
-		log.Fatalf("%v\n", err)
+		log.Fatalf("WorkTree: %v\n", err)
 	}
 
-	start := time.Date(2017, time.September, 2, 12, 0, 0, 0, time.UTC)
+	start, err := time.Parse("Jan 2 2006", os.Args[2])
+	if err != nil {
+		log.Fatalf("Invalid time: %v\n", err)
+	}
+	start = start.Add(time.Hour * 12)
 
-	for i := 0; i < 10; i++ {
-		wt.Commit("WritHub Pixel ART", &git.CommitOptions{
-			Author: &object.Signature{
-				When: start.Add(time.Hour * 24 * time.Duration(i)),
-			},
-		})
+	img := image.NewAlpha(image.Rect(0, 0, 42, 7))
+	width, height := img.Rect.Dx(), img.Rect.Dy()
+
+	tiny.Font.DrawString(img, 0, 1, os.Args[3], color.Black)
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			if img.Pix[y*width+x] == 255 {
+				wt.Commit("WritHub Pixel ART", &git.CommitOptions{
+					Author: &object.Signature{
+						Name:  os.Args[4],
+						Email: os.Args[5],
+						When:  start.Add(time.Hour * 24 * time.Duration(x*height+y)),
+					},
+				})
+			}
+		}
 	}
 
 	ref, err := repo.Head()
 	if err != nil {
-		log.Fatalf("%v\n", err)
+		log.Fatalf("Head: %v\n", err)
 	}
 
 	cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
